c5df9bdf7654897f08b8055591ff6c805466537d
MrLincon <52606514+MrLincon@users.noreply.github.com>
2019-10-08 13:55:56 +0600

    Create test.cpp

---
+++ Test/test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+     int choice;
+     cout<<"Enter your choice: ";
+     cin>>choice;
+
+     if(choice==1){
+        string str1,str2;
+        cout<<"Enter string 1: ";
+        getline(cin,str1);
+        cout<<"\n";
+
+        cout<<"Enter string 2: ";
+        getline(cin,str2);
+        cout<<"\n";
+
+        cout<<"String: "<<str1+" "+str2;
+        cout<<"\n";
+    }
+
+}
