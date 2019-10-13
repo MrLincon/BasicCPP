5f9347e571d0cdbcf63d36890891abb1852485d9
MrLincon <52606514+MrLincon@users.noreply.github.com>
2019-10-14 03:03:44 +0600

    Create jump_statements.cpp

---
+++ jump_statements.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i;
+    int j;
+    int p;
+    string choice;
+
+    lable:
+    cout<< "1. Continue + Jump"<<endl;
+    cout<< "2. Break + Jump"<<endl;
+    cout<< "\n";
+
+    cout<< "Choose 1 or 2: ";
+    cin>>p;
+    cout<< "\n";
+
+    cout<< "Enter the number you want to exclude between 1-10: ";
+    cin>>j;
+    cout<< "\n";
+
+    if(p==1){
+        for(i=1;i<=10;i++){
+            if(i==j){
+                continue;
+            }else{
+                cout<<i<<" ";
+            }
+        }
+    }else if(p==2){
+        for(i=1;i<=10;i++){
+            if(i==j){
+                break;
+            }else{
+                cout<<i<<" ";
+            }
+        }
+    }
+
+
+    cout<< "\n"<<endl;
+    cout<< "Do you want to select again? Y/N  ";
+    cin>>choice;
+    cout<< "\n";
+    if(choice == "Y" || choice == "y"){
+        goto lable;
+        cout<< "\n";
+    }else{
+        cout<< "Thank You!"<<endl;
+    }
+}
+
+
+
+
+
