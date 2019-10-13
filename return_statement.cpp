d1d3df62b507b026e92a4f13922458d81a711cde
MrLincon <52606514+MrLincon@users.noreply.github.com>
2019-10-14 03:14:44 +0600

    Create return_statement.cpp

---
+++ return_statement.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int SUM(int a, int b){
+    int sum = a+b;
+    return sum;
+}
+
+int main(){
+    int x,y;
+    cout<< "Enter two numbers:"<<endl;
+    cout<< "\n";
+    cin>>x;
+    cout<< "\n";
+    cin>>y;
+    cout<< "\n";
+
+    int sum = SUM(x,y);
+
+    cout<< "Sum: "<<sum<<endl;
+
+
+}
